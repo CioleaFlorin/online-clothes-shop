1655965243dd23ac03692d9c4c0254c88e486614
Florin <ciolea.florin.99@gmail.com>
2020-01-04 17:36:57 +0200

    Initial commit (project structure)

---
+++ src/main/java/org/fasttrackit/onlineclothesshop/OnlineClothesShopApplication.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.onlineclothesshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineClothesShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineClothesShopApplication.class, args);
+	}
+
+}
